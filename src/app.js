1b41e05f41617ad7a98c2169b21a6830a92232ca
senpaiii1312 <spatil13122004@gmail.com>
2026-02-05 14:24:11 +0530

    Utils code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app  = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// express configurations
+app.use(express.json({limit: "16kb"})) // json file input, size limit
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // data coming from url
+app.use(express.static("public")) // for public folder
+app.use(cookieParser())
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ import "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch( (err) => {
+    console.log("MongoDB Connection Error!", err);
+})
